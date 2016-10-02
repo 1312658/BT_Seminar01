e7961c8ea311eea3978891ed45aa7cd57decc382
NguyenHoangAnhDung <1312092@student.hcmus.edu.vn>
2016-10-03 00:19:40 +0700

    Design Backend
    Tạo các phương thức CRUD cho blog

---
+++ SkyBlog/db/migrate/20161002133313_add_is_published_to_blog_post.rb
@@ -0,0 +1,10 @@
+class AddIsPublishedToBlogPost < ActiveRecord::Migration
+  def up
+      add_column :blog_posts, :ispublished, :boolean
+  end
+
+  def down
+    remove_column :blog_posts, :ispublished
+  end
+
+end
